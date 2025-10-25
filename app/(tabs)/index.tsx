271936555b05224820b8a7eaba10c78116df7eb5
sourabh151 <jhamnanisourabh2@gmail.com>
2025-10-25 10:47:07 +0530

    cfpc

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,49 +1,34 @@
 import { createHomeStyles } from '@/assets/styles/home.styles'
+import Header from '@/components/Header'
 import { useTasks } from '@/hooks/useData'
 import { useTheme } from '@/hooks/useTheme'
 import { LinearGradient } from 'expo-linear-gradient'
 import React, { useEffect } from 'react'
-import { StatusBar, Text, TouchableOpacity } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar, TouchableOpacity } from 'react-native'
 
 
 
 const Index = () => {
-  const { colors, toggleDarkMode } = useTheme()
-  const { tasks, getAllTasks, addTask } = useTasks();
+    const { colors, toggleDarkMode } = useTheme()
+    const { tasks, getAllTasks, addTask } = useTasks();
 
-  const styles = createHomeStyles(colors);
+    const styles = createHomeStyles(colors);
 
-  useEffect(() => {
-    getAllTasks()
-  }, [])
+    useEffect(() => {
+        getAllTasks()
+    }, [])
 
 
-  return (
-    <LinearGradient
-      colors={colors.gradients.background}>
-      <StatusBar barStyle={colors.statusBarStyle} />
-      <SafeAreaView style={styles.safeArea}>
-        <Text style={{ paddingTop: 40, color: colors.text }}>
-          {tasks[0]?.name}
-        </Text>
-        <TouchableOpacity style={styles.title}>
-          <Text onPress={() => {
-            toggleDarkMode()
-          }}>
-            change theme
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text onPress={() => {
-            addTask("New Task from index")
-          }}>
-            add task
-          </Text>
-        </TouchableOpacity>
-      </SafeAreaView>
-    </LinearGradient>
-  )
+    return (
+        // <SafeAreaView style={styles.container}>
+        // </SafeAreaView>
+        <LinearGradient colors={colors.gradients.primary} style={styles.container}>
+            <StatusBar backgroundColor={colors.bg} barStyle={colors.statusBarStyle} />
+            <TouchableOpacity onPress={toggleDarkMode} style={styles.header}>
+                <Header />
+            </TouchableOpacity>
+        </LinearGradient>
+    )
 }
 
 
---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+import { createHomeStyles } from '@/assets/styles/home.styles';
+import { useTasks } from '@/hooks/useData';
+import { useTheme } from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+    const { colors } = useTheme();
+    const styles = createHomeStyles(colors);
+    const { getAllTasks, tasks } = useTasks()
+    const completed = tasks.filter(task => task.completed).length
+    const progressPercentage = tasks.length === 0 ? 0 : (completed / tasks.length) * 100
+    return (
+        <View style={styles.header}>
+            <View style={styles.titleContainer}>
+                <LinearGradient colors={Object.values(colors.gradients.primary)} style={styles.iconContainer}>
+                    <Ionicons name='flash-outline' size={28} color="#fff" />
+                </LinearGradient>
+                <View style={styles.titleTextContainer} >
+                    <Text style={styles.title}>Today's Tasks 👀</Text>
+                    <Text style={styles.subtitle}>{`${completed} of ${tasks.length} completed`}</Text>
+                </View>
+            </View>
+
+            <View style={styles.progressContainer}>
+                <View style={styles.progressBarContainer}>
+                    <View style={styles.progressBar}>
+                        <LinearGradient colors={colors.gradients.success} style={[styles.progressFill, { width: `${progressPercentage}%` }]}>
+                        </LinearGradient>
+                    </View>
+                    <Text style={styles.progressText}>{Math.round(progressPercentage)}%</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default Header
