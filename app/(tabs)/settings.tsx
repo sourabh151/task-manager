d6ac71c66e55804b5e52c54d16fa252acfad5320
sourabh jhamnani <jhamnanisourabh2@gmail.com>
2025-11-11 23:39:23 +0530

    cfm

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { colors } from "@/assets/colors"
+import { Ionicons } from '@expo/vector-icons'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const settingsScreen = () => {
-    return (
-        <View>
-            <Text>settingsScreen</Text>
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Ionicons style={styles.header_side} name="apps-outline" size={20} />
         </View>
-    )
+      </View>
+    </SafeAreaView>
+  )
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: colors.iconPrivacy,
+    height: 250
+  },
+  header: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  header_center: {
+    flex: 1
+  },
+  header_side: {
+    width: 20,
+    backgroundColor: colors.iconAppearance,
+    color: colors.divider
+  }
+})
 
 export default settingsScreen
---
+++ assets/colors.ts
@@ -0,0 +1,23 @@
+
+export const colors = {
+  background: "#f6f6f6",           // Light gray for main background
+  card: "#ffffff",                 // White card and cell backgrounds
+  accent: "#0088cc",               // Telegram blue (header, main accent, active tab)
+  textPrimary: "#222222",          // Main profile name (black)
+  textSecondary: "#888888",        // Sub-labels/icons/text (gray)
+  iconBackground: "#e3e3e7",       // Light background for some icons
+  notificationRed: "#d8232a",      // Notification dot (red)
+  divider: "#ebebeb",              // Light divider lines
+  tabInactive: "#cccccc",          // Inactive tab icon color
+  tabActive: "#0088cc",            // Active tab icon color
+  iconProfile: "#ed424a",          // Profile icon color (red/pink)
+  iconSavedMessages: "#2887e3",    // Saved Messages icon (blue)
+  iconRecentCalls: "#38c349",      // Recent Calls icon (green)
+  iconDevices: "#d38f29",          // Devices icon (orange)
+  iconChatFolders: "#349bad",      // Chat Folders icon (cyan/blue)
+  iconNotifications: "#ed424a",    // Notifications icon (red/pink)
+  iconPrivacy: "#8899a7",          // Privacy icon (gray/blue)
+  iconData: "#41c2aa",             // Data and Storage icon (teal)
+  iconAppearance: "#1c6ecb",       // Appearance icon (blue)
+  iconPowerSaving: "#d38f29",      // Power Saving icon (deep orange)
+};
